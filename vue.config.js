bfc78c3cb959bfc39cc434737c04dbd0c2e4fba3
aurora <1952153185@qq.com>
2022-03-31 00:49:09 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   // publicPath: "/usr/share/nginx/html/",
 };
+
+//docker run -d -p 80:80 -v /home/aurora/vue:/usr/share/nginx/html/ -v /home/aurora/nginx:/etc/nginx --name="vue" --restart="on-failure" nginx
